3d53c687e6a1b2d136ed0edc99daf5b6c780ffd1
«Mansur» <sozd.space@gmail.com>
2022-08-30 23:48:26 +0300

    написал класс company и интегрировал google map

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
+import { Company } from './Company'
 import { User } from "./User"
 
 const user = new User()
-
+const company = new Company()
+//  new google.maps.Map(document.getElementById("map"))
